e19479e8a859c063381d29a39ce31819efa41b59
RogeWood <a813712100@gmail.com>
2019-12-22 17:52:58 +0800

    Add program notes and README

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # PingPongBallGame
 使用: python語法 & pygame
 遊戲內容: 以擋板控制球，使求不要超出窗外
+
+**第一次用pygame寫遊戲**
+
 ## 想法
 球撞到擋板會分裂成兩顆
-### 語法寫法
+### 程式碼
+1. class建立球的屬性
+2. 判斷collision是否為擋板
+3. 將新增的球存入balls(list)中
+4. 更新畫面時以for跑每個球物件
--- ping!pong!ball!/ping!pong!ball!.py
+++ ping!pong!ball!/ping!pong!ball!.py
@@ -3,7 +3,8 @@ from pygame.locals import *
 from pygame.math import Vector2
 from random import randint
 
-pygame.init()
+#初始設定
+pygame.init() #模組初始化
 screen = pygame.display.set_mode((640,480))
 icon = pygame.image.load("image/icon.png")
 pygame.display.set_icon(icon)
@@ -12,6 +13,7 @@ fpsclock = pygame.time.Clock()
 FPS = 60
 running = True
 
+#擋板變數
 barW = 60
 barH = 30
 barP = Vector2(200,460)
@@ -20,7 +22,7 @@ score = 0
 hitBar = False
 gameOver = True
 
-balls = []
+balls = [] #存球
 
 class Ball(pygame.sprite.Sprite):
     def __init__(self,spx,spy,dx,dy,red,green,blue):
@@ -31,21 +33,21 @@ class Ball(pygame.sprite.Sprite):
         self.HasCreatNewBall = True
         pygame.mixer.music.load("audio/ball_collision.mp3")
 
-    def moveBall(self):
+    def moveBall(self): #球的移動
         self.ballP += self.ballV
 
-    def BallCollision(self,barPx,barPy,barW,barH):
+    def BallCollision(self,barPx,barPy,barW,barH): #碰撞偵測
         hitBar = False
 
         dis = self.ballP+self.ballV - (barPx,barPy)
-        if (abs(dis.x) < barW/2) and (abs(dis.y) < barH/2):
+        if (abs(dis.x) < barW/2) and (abs(dis.y) < barH/2): #是否有撞到擋板
             hitBar = True
 
-        if self.ballP.x > screen.get_width() or self.ballP.x < 0:
+        if self.ballP.x > screen.get_width() or self.ballP.x < 0: #當撞到兩側
             pygame.mixer.music.play()
             self.ballV.x *= -1
             self.HasCreatNewBall = False
-        if hitBar or self.ballP.y < 0:
+        if hitBar or self.ballP.y < 0: #當撞到擋板和頂端
             if hitBar and self.HasCreatNewBall == False:
                 pygame.mixer.music.play()
                 self.ballV.y *= -1
@@ -56,30 +58,30 @@ class Ball(pygame.sprite.Sprite):
                 self.ballV.y *= -1
                 self.HasCreatNewBall = False
 
-        elif self.ballP.y > screen.get_height():
+        elif self.ballP.y > screen.get_height(): #當超出視窗
             balls.clear()
             global gameOver
-            gameOver = True
+            gameOver = True #遊戲結束
 
-    def creatNewBall(self):
+    def creatNewBall(self): #新增球
         global score
         score += 1
         item = Ball(self.ballP.x,self.ballP.y-15,randint(2,5),randint(2,5)*-1,randint(50,255),randint(50,255),randint(0,200))
         balls.append(item)
 
-    def BallUpdate(self,barPx,barPy,barW,barH):
+    def BallUpdate(self,barPx,barPy,barW,barH): #更新球的狀況
         self.moveBall()
         self.BallCollision(barPx,barPy,barW,barH)
         pygame.draw.circle(screen, self.ballColor, [int(self.ballP.x),int(self.ballP.y)],10)
 
 
-def GameStart():
+def GameStart(): #遊戲進行中
     screen.fill((0,0,255))
 
     for ball in balls:
         ball.BallUpdate(barP.x,barP.y,barW,barH)
 
-def Menu():
+def Menu(): #遊戲結束菜單
     screen.fill((0,0,0))
     menuFont = pygame.font.Font("font/Roboto-Black.ttf",24)
     printPlayAgain = menuFont.render("Press the space bar to play",True,(255,255,255))
@@ -93,7 +95,7 @@ while running:
         if event.type == KEYDOWN:
             if event.key == K_ESCAPE:
                 running = False
-            elif event.key == K_SPACE and gameOver:
+            elif event.key == K_SPACE and gameOver: #按下空白鍵使遊戲開始
                 item = Ball(0,0,5,3,255,50,50)
                 balls.append(item)
                 score = 0
@@ -106,6 +108,7 @@ while running:
     else:
         Menu()
 
+    #擋板更新
     mouseX, mouseY = pygame.mouse.get_pos()
     barP.x = max(min(mouseX,screen.get_width()),0)
     pygame.draw.rect(screen, (200,200,0), [barP.x-barW/2,barP.y-barH/2,barW,barH])
